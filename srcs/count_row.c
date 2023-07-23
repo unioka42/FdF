720d65ea999cfd0a8ed4e7bb750c600fdeb5c94c
Koki Okada <uniokaworld@gmail.com>
2023-07-23 13:58:48 +0900

    add count_row

---
+++ srcs/count_row.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   count_row.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kokada <kokada@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/23 00:20:14 by kokada            #+#    #+#             */
+/*   Updated: 2023/07/23 02:45:18 by kokada           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+int	count_row(char *file_path)
+{
+	int		fd;
+	int		count;
+	char	*line;
+
+	fd = open(file_path, O_RDONLY);
+	count = 0;
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (line == NULL)
+			return (count);
+		count++;
+		free(line);
+	}
+}
