7e07715ae1ca29d3d90fa73c84807cef10701f20
Koki Okada <uniokaworld@gmail.com>
2023-08-18 21:08:49 +0900

    pose init

--- srcs/initialize.c
+++ srcs/initialize.c
@@ -6,7 +6,7 @@
 /*   By: kokada <kokada@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 22:44:32 by kokada            #+#    #+#             */
-/*   Updated: 2023/08/16 10:28:52 by kokada           ###   ########.fr       */
+/*   Updated: 2023/08/18 19:32:14 by kokada           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ int	allocate_memory(t_fdf **fdf_ptr)
 			(*fdf_ptr)->display = (t_display *)malloc(sizeof(t_display));
 			if ((*fdf_ptr)->display != NULL)
 			{
-				(*fdf_ptr)->camera = (t_camera *)malloc(sizeof(t_camera));
-				if ((*fdf_ptr)->camera != NULL)
+				(*fdf_ptr)->pose = (t_pose *)malloc(sizeof(t_pose));
+				if ((*fdf_ptr)->pose != NULL)
 					return (1);
 				free((*fdf_ptr)->display);
 			}
@@ -33,4 +33,27 @@ int	allocate_memory(t_fdf **fdf_ptr)
 		free((*fdf_ptr));
 	}
 	return (-1);
+}
+
+int	mlx_display_init(t_display *display)
+{
+	display->mlx = mlx_init();
+	display->win = mlx_new_window(display->mlx, WIDTH, HEIGHT, "FdF");
+	display->img = mlx_new_image(display->mlx, WIDTH, HEIGHT);
+	display->data_addr = mlx_get_data_addr(display->img,
+		&(display->bits_per_pixel), &(display->size_line), &(display->endian));
+	return (0);
+}
+
+int	pose_init(t_fdf *fdf)
+{
+	fdf->pose->positon_x = 0;
+	fdf->pose->positon_y = 0;
+	fdf->pose->positon_z = ko_min(WIDTH / fdf->map->width / 2.5, HEIGHT
+		/ fdf->map->height / 2.5);
+	fdf->pose->rotate_x = 0;
+	fdf->pose->rotate_y = 0;
+	fdf->pose->rotate_z = 0;
+	fdf->pose->height = 0.1;
+	return (1);
 }
