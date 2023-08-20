0a4741f6756b944700f7a9c926fa10777ce571e9
Koki Okada <uniokaworld@gmail.com>
2023-08-20 14:26:38 +0900

    feat: main file

--- srcs/main.c
+++ srcs/main.c
@@ -6,48 +6,26 @@
 /*   By: kokada <kokada@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 14:46:07 by kokada            #+#    #+#             */
-/*   Updated: 2023/07/24 22:45:37 by kokada           ###   ########.fr       */
+/*   Updated: 2023/08/20 14:25:53 by kokada           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
-#include <stdio.h>
-
-int	deal_key(int key, t_data *data)
-{
-	ft_putnbr_fd(key, 1);
-	if (key == 126)
-		data->display->shift_y -= 10;
-	if (key == 125)
-		data->display->shift_y += 10;
-	if (key == 123)
-		data->display->shift_x -= 10;
-	if (key == 124)
-		data->display->shift_x += 10;
-	if (key == 6)
-		data->display->a -= 0.05;
-	if (key == 7)
-		data->display->a += 0.05;
-	mlx_clear_window(data->display->mlx, data->display->mlx_win);
-	draw(data->map, data->display);
-	return (0);
-}
 
 int	main(int argc, char **argv)
 {
-	t_data	*data;
+	t_fdf	*fdf;
 
-	// if (argc != 2)
-	// 	ko_error("usege: ./fdf map_file.fdf");
-	data = (t_data *)malloc(sizeof(t_data));
-	data->map = read_map("./42.fdf");
-	data->display = (t_display *)malloc(sizeof(t_display));
-	data->display->mlx = mlx_init();
-	data->display->mlx_win = mlx_new_window(data->display->mlx, 1000, 1000,
-		"fdf");
-	data->display->zoom = 20;
-	draw(data->map, data->display);
-	mlx_key_hook(data->display->mlx_win, deal_key, data);
-	mlx_loop(data->display->mlx);
+	if (argc != 2)
+		ko_error("usege: ./fdf map_file.fdf");
+	if (allocate_memory(&fdf) < 0)
+		ko_error("malloc err");
+	if (read_map(argv[1], fdf->map) < 0)
+		ko_error("file map err");
+	mlx_display_init(fdf->display);
+	pose_init(fdf);
+	draws(fdf);
+	mlx_hook_init(fdf);
+	mlx_loop(fdf->display->mlx);
 	return (0);
 }
