40c6d9321c5dbca470aa287fa00b26176b615af3
Koki Okada <uniokaworld@gmail.com>
2023-09-13 15:11:14 +0900

    feat

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: kokada <kokada@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 11:11:47 by kokada            #+#    #+#             */
-/*   Updated: 2023/08/22 23:52:08 by kokada           ###   ########.fr       */
+/*   Updated: 2023/09/13 14:57:47 by kokada           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,7 @@
 
 void	ko_error(char *error_msg, t_fdf *fdf)
 {
-	ft_putstr_fd(error_msg, 1);
-	ft_putchar_fd('\n', 1);
+	ft_putendl_fd(error_msg, 1);
 	if (fdf != NULL)
 		free_fdf(fdf);
 	exit(1);
